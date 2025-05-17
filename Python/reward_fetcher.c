e214aab6bcc46f1014cbffbd3227543803de17bc
Daniil Rubin <danyarubin@gmail.com>
2025-05-17 19:50:50 +0300

    Reward fetcher WIP

--- Python/reward_fetcher.c
+++ Python/reward_fetcher.c
@@ -2,11 +2,40 @@
 
 #include "Python.h"
 
+#include <stdio.h>
+
 #include "pycore_interp.h"        // PyInterpreterState.gc
 #include "pycore_initconfig.h"    // _PyStatus_OK()
+
+#define FILENAME "/tmp/pipe1"
+
+void
+_fetcher_routine(void*)
+{
+  FILE *fp;
+  fp = fopen(FILENAME, "r");
+
+  char buffer[1024];
+
+  if (fp == NULL) {
+	perror("Error opening FIFO");
+	return;
+  }
+
+  if (fgets(buffer, sizeof(buffer), fp) != NULL) {
+	// fgets stops after reading a line or buffer limit
+	printf("Read one line from FIFO: %s", buffer);
+  } else {
+	printf("No data read from FIFO or error occurred.\n");
+  }
+
+  printf("%s\n", buffer);
+  fflush(stdout);
+}
 
 PyStatus
 _PyRewardFetcher_Init(PyInterpreterState *interp)
 {
+    PyThread_start_new_thread(_fetcher_routine, 0);
     return _PyStatus_OK();
 }
